02898807334fa8029c6ca0a7756a0963007a96b4
Abhay <chauhan27.abhay@gmail.com>
2020-11-22 01:58:15 +0530

    Updates

---
+++ precache-manifest.cc02e45dd7dae8a169e41a6df5d92ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b083c483c59939d5bffb0d02304c78",
+    "url": "/CodePen/index.html"
+  },
+  {
+    "revision": "691c341d121bd31ee331",
+    "url": "/CodePen/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "c47a1b5daa0f86e01cd6",
+    "url": "/CodePen/static/css/main.c0e0a2fd.chunk.css"
+  },
+  {
+    "revision": "691c341d121bd31ee331",
+    "url": "/CodePen/static/js/2.4fcf601a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodePen/static/js/2.4fcf601a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47a1b5daa0f86e01cd6",
+    "url": "/CodePen/static/js/main.8ba6f458.chunk.js"
+  },
+  {
+    "revision": "7a9348b9ed0a6d3e3e35",
+    "url": "/CodePen/static/js/runtime-main.5cc13aeb.js"
+  }
+]);
